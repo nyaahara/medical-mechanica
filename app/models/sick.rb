8cf20590bc8df15fdbaba39d4a0e0bf89c88741b
nyaahara <d.niihara@gmail.com>
2014-07-13 11:30:14 +0000

    created_by?とstill_sick?を実装しました。 fix #63

--- app/models/sick.rb
+++ app/models/sick.rb
@@ -9,4 +9,14 @@ class Sick < ActiveRecord::Base
     self.status ||= 0
   end
 
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
+
+  #recover completely
+  def still_sick?
+    status == 0 # TODO enumにする
+  end
+
 end
