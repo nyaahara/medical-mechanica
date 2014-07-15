09e536eaf3519b447026f6befbd7e10f0373ff27
nyaahara <d.niihara@gmail.com>
2014-07-15 17:37:19 +0000

    idを選択できるようにした。 メモか登録日時を選択こうもくとしたらわかりやすくなりそうだ。 fix #71

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -6,14 +6,19 @@ class ProgressesController < ApplicationController
     now = Time.zone.now
     @progress = current_user.progresses.build
     @progress.progress_at = now - now.sec
+    @still_sicks = Sick.where(:owner_id => current_user.id, :status => 0)
   end
 
   def create
     
-    sick = Sick.find_or_create_by(:owner_id => current_user.id, :status => 0)
-    # sick.save!
+    permited_params = progress_params
+    sick = if permited_params[:sick_id]
+      Sick.find(permited_params[:sick_id])
+    else
+      Sick.create(:owner_id => current_user.id)
+    end
 
-    @progress = sick.progresses.build(progress_params) do |p|
+    @progress = sick.progresses.build(permited_params) do |p|
       p.user_id = current_user.id
       p.parts.each do |part|
         part.user_id = current_user.id
@@ -35,7 +40,7 @@ class ProgressesController < ApplicationController
   private
 
   def progress_params
-    params.require(:progress).permit(:progress_at, :parts_attributes => [:part, :kind, :level, :x, :y, :_destroy])
+    params.require(:progress).permit(:sick_id, :progress_at, :parts_attributes => [:part, :kind, :level, :x, :y, :_destroy])
   end
 
 end
