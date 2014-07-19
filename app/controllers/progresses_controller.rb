66a6848bbaeedc07803a0c6459b137d3041a5849
nyaahara <d.niihara@gmail.com>
2014-07-19 17:30:24 +0000

    メインへ戻ろう。 fix #78

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -4,26 +4,14 @@ class ProgressesController < ApplicationController
 
   def create
     
+    # 親のsickがなければ、新しく作ります。
     @sick = Sick.find_or_create_by(:owner_id => current_user.id, :status => 0)
-    permited_params = sssss(progress_params,@sick)
-    # permited_params = progress_params
-    # @sick = if permited_params[:sick_id].present?
-    #   Sick.find(permited_params[:sick_id])
-    # else
-    #   Sick.create(:owner_id => current_user.id)
-    # end
-    
-    @sick.label = params[:sick][:label]
     @sick.save!
 
+    # Strong parameterとキーの設定を行います。
+    permited_params = progress_params(@sick)
+    
     @progress = @sick.progresses.build(permited_params)
-    # @progress = @sick.progresses.build(permited_params) do |p|
-    #   p.user_id = current_user.id
-    #   p.parts.each do |part|
-    #     part.user_id = current_user.id
-    #     part.sick_id = @sick.id
-    #   end
-    # end
 
     if @progress.save
       redirect_to @sick, notice: '登録しました'
@@ -42,19 +30,20 @@ class ProgressesController < ApplicationController
 
   def update
 
-    # TODO sickだけ検索して、関連を引きづり出す方法があるはずだよ？
-    @progress = Progress.find(params[:id])
-    @sick = Sick.find(@progress.sick_id)
+    # 親のsickと一緒にprogressを取得します
+    @progress = Progress.includes(:sick).find(params[:id])
 
     # 保存されたpartsを一旦削除する
     @progress.parts.each do |p|
       p.destroy!
     end
 
-    permited_params = sssss(progress_params,@sick)
+    # Strong parameterとキーの設定を行います。
+    permited_params = progress_params(@progress.sick)
 
+    # データを更新します。
     if @progress.update(permited_params)
-      redirect_to @sick, notice:'編集しました'
+      redirect_to @progress.sick, notice:'編集しました'
     else
       render :new
     end
@@ -62,17 +51,14 @@ class ProgressesController < ApplicationController
 
   private
 
-  def sssss(data,sick)
-    data[:user_id] = sick.owner_id
-    data[:parts_attributes].each do |pp|
-      pp[1][:user_id] = sick.owner_id
-      pp[1][:sick_id] = sick.id
+  def progress_params(sick)
+    permited = params.require(:progress).permit(:progress_at, :parts_attributes => [:part, :kind, :level, :x, :y, :_destroy])
+    permited[:user_id] = sick.owner_id
+    permited[:parts_attributes].each do |part|
+      part[1][:user_id] = sick.owner_id
+      part[1][:sick_id] = sick.id
     end
-    data
-  end
-
-  def progress_params
-    params.require(:progress).permit(:sick_id, :progress_at, :parts_attributes => [:part, :kind, :level, :x, :y, :_destroy])
+    permited
   end
 
 end
