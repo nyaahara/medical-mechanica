5a48252275b93b508da151ca90c8fc2cf47aaa7b
nyaahara <d.niihara@gmail.com>
2014-07-20 03:27:31 +0000

    レイアウト変更の順番を変更します。先に症状のところを奇麗にしてから、一覧だ。 理由は、一覧画面に表示する内容に迷ってるから。 症状１個１個のレイアウトとかに寄ると思うんよ。結局。 fix #83

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @sicks = Sick.where(1).order(:created_at)
+                                                      # TODO desc
+                                                          # TODO limit 
+    @progresses = Progress.includes(:sick).where(1).order(:created_at)
+    @users = User.where(1).order(:created_at)
   end
 end
