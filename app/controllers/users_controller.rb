b9d3c7439ba732e5dab9a9702ebf0b2b91866b90
nyaahara <d.niihara@gmail.com>
2014-07-27 13:22:16 +0000

    とりあえず、更新できるようになった #95

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
     @sicks = Sick.where(:owner_id => owner_id).find_each
   end
 
+  def edit
+    owner_id = params[:id]
+    @user = User.find(owner_id)
+  end
+
+  def update
+    @user = current_user    
+    if @user.update(user_params)
+      redirect_to @user, notice: '更新しました'
+    else
+      render :edit
+    end
+  end
+  
+  private 
+
+  def user_params
+    params.require(:user).permit(:birth,:sex)
+  end
+
 end
