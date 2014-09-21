9c8216bd37218fe6d60d3c2ddea7f9fc41bcf0b0
nyaahara <d.niihara@gmail.com>
2014-09-21 11:07:17 +0900

    update method and destroy method have not id params. so add it.

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -34,7 +34,7 @@ class SymptomsController < ApplicationController
     end.length == 0
 
     if @symptom.update(param)
-      redirect_to controller: 'users', action: 'show', notice: '更新しました'
+      redirect_to controller: 'users', action: 'show', id: current_user.id, notice: '更新しました'
     else
       render :edit
     end
@@ -43,7 +43,7 @@ class SymptomsController < ApplicationController
   def destroy
     @symptom = current_user.symptom.find(params[:id])
     if @symptom.destroy!
-      redirect_to controller: 'users', action: 'show', notice: '削除しました'
+      redirect_to controller: 'users', action: 'show', id: current_user.id, notice: '削除しました'
     end
   end
 
