1e9b7a5edcd01f222254875b12a3f107991b5a6d
nyaahara <d.niihara@gmail.com>
2014-09-26 01:32:19 +0900

    Twitter's icon reload when user login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    user.update({image_url: request.env['omniauth.auth'][:info][:image]})
     session[:user_id] = user.id
     redirect_to root_path, notice: 'ログインしました'
   end
