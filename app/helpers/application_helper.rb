73754b3c15f9bbece59b2a50c11a69ebe02c8e90
nyaahara <d.niihara@gmail.com>
2014-09-25 23:47:04 +0900

    Add Tweet Link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def url_for_twitter(user)
+    "https://twitter.com/#{user.nickname}"
+  end
+
+  def url_for_symptom(symptom)
+    "http://localhost:3000/users/#{symptom.user_id}/symptoms/#{symptom.id}"
+  end
 end
