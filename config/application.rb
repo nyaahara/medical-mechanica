52f0f455e4570b8e98db12266be1bdcc3278d71e
nyaahara <d.niihara@gmail.com>
2014-11-09 16:48:35 +0900

    エラーメッセージを日本語かしました。

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module MedicalMechanica
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
