42b0400af677d1d9d54557586e1d47ad8d55becd
nyaahara <d.niihara@gmail.com>
2014-11-13 22:36:56 +0900

    the user_spec.rb has been completed.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,4 +7,21 @@ FactoryGirl.define do
     sequence(:nickname) { |i| "nickname#{i}" }
     sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
   end
+
+  ## class: Userを入れないとインスタンスメソッドのテストが出来ないようです。
+  factory :twitter_user, class: User do
+    sequence(:nickname) { |i| "nickname#{i}" }
+    sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
+    after(:create) do|user|
+      FactoryGirl.create(:twitter_auth, user: user)
+    end
+  end
+
+  factory :facebook_user, class: User do
+    sequence(:nickname) { |i| "nickname#{i}" }
+    sequence(:image_url) { |i| "http://example.com/image#{i}.jpg" }
+    after(:create) do|user|
+      FactoryGirl.create(:facebook_auth, user: user)
+    end
+  end
 end
--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Auth, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe '#create_from_auth_hash' do
+
+  end
 end
